40a5e0a0e76217b6543591e4967167ad7045be24
CREDO23 <thierrybakera12@gmail.com>
2022-11-03 15:18:30 +0200

    setup : tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import './App.css';
-
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <div className="App">
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+        <div className="w-[100vw] h-[100vh] bg-orange-400 flex items-center justify-center flex-col">
+            <h4 className="">Welcome back !</h4>
         </div>
     );
 }

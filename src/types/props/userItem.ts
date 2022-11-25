eb11f51baa7a030dde0824fe59c42e41daeec28f
CREDO23 <thierrybakera12@gmail.com>
2022-11-25 10:20:05 +0200

    fix : reset query after research user

--- src/types/props/userItem.ts
+++ src/types/props/userItem.ts
@@ -3,11 +3,13 @@ import USER from '../user';
 
 type UserItem = {
     userName: string;
+    email: string;
     id: string;
     avatar?: string;
     online: boolean;
     mode?: 'private' | 'channel';
     setUserChat: React.Dispatch<React.SetStateAction<USER[]>>;
+    setSearch: React.Dispatch<React.SetStateAction<string>>;
     setRightSide?: React.Dispatch<React.SetStateAction<'users' | 'me'>>;
     setMainSide?: React.Dispatch<
         React.SetStateAction<'chats' | 'users' | 'messages' | 'profil'>
